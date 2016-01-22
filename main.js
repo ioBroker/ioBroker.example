d822e745c2dbff5fdc6746909bbb325ed0c79e9f
foxthefox <github@wysiwis.net>
2016-01-22 22:37:46 +0100

    Update main.js

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ var utils =    require(__dirname + '/lib/utils'); // Get common adapter utils
 // you have to call the adapter function and pass a options object
 // name has to be set and has to be equal to adapters folder name and main file name excluding extension
 // adapter will be restarted automatically every time as the configuration changed, e.g system.adapter.template.0
-var adapter = utils.adapter('template');
+var adapter = utils.adapter('jeelink');
 
 // is called when adapter shuts down - callback has to be called under any circumstances!
 adapter.on('unload', function (callback) {
