43fc33a2f6a8093a5323f203bb4fc587a99dc25d
foxthefox <github@wysiwis.net>
2016-01-23 13:24:00 +0100

    Update jeelink.js

--- jeelink.js
+++ jeelink.js
@@ -80,32 +80,6 @@ function main() {
         baudrate:       adapter.config.baudrate   || 57600,
     };
 
-    var SerialPort = require("serialport").SerialPort
-    var serialPort = new SerialPort(options:serialport, { baudrate: 57600
-    }, false); // this is the openImmediately flag [default is true]
-
-    serialPort.open(function (error) {
-        if ( error ) {
-           console.log('failed to open: '+error);
-          } else {
-       console.log('open');
-            serialPort.on('data', function(data) {
-          console.log('data received: ' + data);
-        });
-    serialPort.write("ls\n", function(err, results) {
-          console.log('err ' + err);
-          console.log('results ' + results);
-        });
-      }
-    });
-
-   serialPort.on("data", function (data) {
-        sys.puts("here: "+data);
-    });
-    
-    
- //   rfm = new rfm(options);
-
     adapter.setObject('testVariable', {
         type: 'state',
         common: {
