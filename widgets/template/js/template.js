5faaf924dbe2d00d08727e1416adfffa8f8e4900
Pmant <patrickmo@gmx.de>
2016-02-05 23:44:07 +0100

    Update template.js
    fix updates

--- widgets/template/js/template.js
+++ widgets/template/js/template.js
@@ -57,7 +57,7 @@ vis.binds.template = {
         $('#' + widgetID).html(text);
 
         // subscribe on updates of value
-        if (vis.states[data.oid]) {
+        if (vis.states[data.oid + '.val']) {
             vis.states.bind(data.oid + '.val', function (e, newVal, oldVal) {
                 $div.find('.template-value').html(newVal);
             });
