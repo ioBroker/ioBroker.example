950938fea1492cd038fa2fe3c12c699014a49789
foxthefox <github@wysiwis.net>
2016-03-03 21:35:02 +0100

    Update jeelink.js

--- jeelink.js
+++ jeelink.js
@@ -2,8 +2,17 @@
 /*jslint node: true */
 "use strict";
 
+var serialport = require("serialport");
+var SerialPort = serialport.SerialPort; // localize object constructor
+var sp = new SerialPort("/dev/ttyUSB0", {
+    baudrate:57600,
+    parser: serialport.parsers.readline('\r\n')
+});
+
+
 // you have to require the utils module and call adapter function
 var utils =    require(__dirname + '/lib/utils'); // Get common adapter utils
+
 
 // you have to call the adapter function and pass a options object
 // name has to be set and has to be equal to adapters folder name and main file name excluding extension
@@ -37,22 +46,10 @@ adapter.on('stateChange', function (id, state) {
     }
 });
 
-// Some message was sent to adapter instance over message box. Used by email, pushover, text2speech, ...
-adapter.on('message', function (obj) {
-    if (typeof obj == 'object' && obj.message) {
-        if (obj.command == 'send') {
-            // e.g. send email or pushover or whatever
-            console.log('send command');
-
-            // Send response in callback if required
-            if (obj.callback) adapter.sendTo(obj.from, obj.command, 'Message received', obj.callback);
-        }
-    }
-});
-
 // is called when databases are connected and adapter received configuration.
 // start here!
 adapter.on('ready', function () {
+        adapter.log.info('entered ready');
     main();
 });
 
@@ -60,33 +57,207 @@ function main() {
 
     // The adapters config (in the instance object everything under the attribute "native") is accessible via
     // adapter.config:
-    adapter.log.info('config test1: ' + adapter.config.test1);
-    adapter.log.info('config test1: ' + adapter.config.test2);
 
+    var obj = adapter.config.sensors;
+    for (var anz in obj){
+        adapter.log.info("anz : " + anz);
+        if(obj.stype==="emonTH") {
+            adapter.setObject('emonTH_' + anz, {
+                type: 'channel',
+                common: {
+                    name: 'emonTH ' + obj[anz].room,
+                    role: 'sensor'
+                },
+                native: {
+                    "addr": anz
+                }
+            });
+            adapter.log.info('RFM12B setting up object ' + anz);
 
-    /**
-     *
-     *      For every state in the system there has to be also an object of type state
-     *
-     *      Here a simple template for a boolean variable named "testVariable"
-     *
-     *      Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
-     *
-     */
+            adapter.setObject('emonTH_' + anz + '.temp', {
+                type: 'state',
+                common: {
+                    "name": "Temperature",
+                    "type": "number",
+                    "unit": "°C",
+                    "min": -50,
+                    "max": 50,
+                    "read": true,
+                    "write": false,
+                    "role": "value.temperature",
+                    "desc": "Temperature"
+                },
+                native: {}
+            });
+            adapter.setObject('emonTH_' + anz + '.humid', {
+                type: 'state',
+                common: {
+                    "name": "Humidity",
+                    "type": "number",
+                    "unit": "%",
+                    "min": 0,
+                    "max": 100,
+                    "read": true,
+                    "write": false,
+                    "role": "value.humidity",
+                    "desc": "Humidity"
+                },
+                native: {}
+            });
+            adapter.setObject('emonTH_' + anz + '.batt', {
+                type: 'state',
+                common: {
+                    "name": "Battery",
+                    "type": "number",
+                    "unit": "V",
+                    "min": 0,
+                    "max": 4,
+                    "read": true,
+                    "write": false,
+                    "role": "value.battery",
+                    "desc": "Battery"
+                },
+                native: {}
+            });
+        }else if(obj.stype==="waterMote"){
+            adapter.setObject('waterMote_' + anz, {
+                type: 'channel',
+                common: {
+                    name: 'waterMote ' + obj[anz].room,
+                    role: 'sensor'
+                },
+                native: {
+                    "addr": anz
+                }
+            });
+            adapter.log.info('RFM12B setting up object ' + anz);
 
+            adapter.setObject('waterMote_' + anz + '.cw_mom', {
+                type: 'state',
+                common: {
+                    "name": "Cold Water",
+                    "type": "number",
+                    "unit": "l",
+                    "min": 0,
+                    "max": 100,
+                    "read": true,
+                    "write": false,
+                    "role": "value",
+                    "desc": "Cold Water"
+                },
+                native: {}
+            });
+            adapter.setObject('waterMote_' + anz + '.cw_cum', {
+                type: 'state',
+                common: {
+                    "name": "Cold Water",
+                    "type": "number",
+                    "unit": "m3",
+                    "min": 0,
+                    "max": 10000,
+                    "read": true,
+                    "write": false,
+                    "role": "value",
+                    "desc": "Cold Water counter"
+                },
+                native: {}
+            });
+            adapter.setObject('waterMote_' + anz + '.ww_mom', {
+                type: 'state',
+                common: {
+                    "name": "Warm Water",
+                    "type": "number",
+                    "unit": "l",
+                    "min": 0,
+                    "max": 100,
+                    "read": true,
+                    "write": false,
+                    "role": "value",
+                    "desc": "Warm Water"
+                },
+                native: {}
+            });
+            adapter.setObject('waterMote_' + anz + '.cw_cum', {
+                type: 'state',
+                common: {
+                    "name": "Warm Water",
+                    "type": "number",
+                    "unit": "m3",
+                    "min": 0,
+                    "max": 10000,
+                    "read": true,
+                    "write": false,
+                    "role": "value",
+                    "desc": "Warm Water counter"
+                },
+                native: {}
+            });
+            adapter.setObject('waterMote_' + anz + '.batt', {
+                type: 'state',
+                common: {
+                    "name": "Battery",
+                    "type": "number",
+                    "unit": "V",
+                    "min": 0,
+                    "max": 4,
+                    "read": true,
+                    "write": false,
+                    "role": "value.battery",
+                    "desc": "Battery"
+                },
+                native: {}
+            });
+        }
+    }
 
     var options = {
         serialport:     adapter.config.serialport || '/dev/ttyUSB0',
         baudrate:       adapter.config.baudrate   || 57600,
     };
 
-    adapter.setObject('testVariable', {
-        type: 'state',
-        common: {
-            type: 'boolean',
-            role: 'indicator'
-        },
-        native: {}
+    sp.open(function (error) {
+        if ( error ) {
+            adapter.log.info('failed to open: '+error);
+        } else {
+            adapter.log.info('open');
+            sp.on('data', function(data) {
+
+                adapter.log.info('data received: ' + data);
+
+                var tmp = data.split(' ');
+                if(tmp[0]==='OK'){
+                    if(tmp[2]==='19'){
+                        //we are expecting data in form \"OK nodeid data1 data2 etc
+                        var tmpp=tmp.splice(3,8);
+                        adapter.log.info('splice:'+tmpp);
+                        var buf = new Buffer(tmpp);
+                        adapter.log.info('Temperature:'+ (buf.readInt16LE(0))/10);
+                        adapter.setState('emonTH_'+ id +'.temp', {val: (buf.readInt16LE(0))/10, ack: true});
+                        adapter.log.info('Humidty: ' + (buf.readInt16LE(4))/10);
+                        adapter.setState('emonTH_'+ id +'.humid', {val: (buf.readInt16LE(4))/10, ack: true});
+                        adapter.log.info('Voltage: ' + (buf.readInt16LE(6))/10);
+                        adapter.setState('emonTH_'+ id +'.batt', {val: (buf.readInt16LE(6))/10, ack: true});
+                    }
+                    else if(tmp[2]==='21' || tmp[2]==='22'){
+                        //we are expecting data in form \"OK nodeid data1 data2 etc
+                        var tmpp=tmp.splice(3,8);
+                        adapter.log.info('splice:'+tmpp);
+                        var buf = new Buffer(tmpp);
+                        adapter.log.info('cw_mom:'+ (buf.readInt16LE(0))/10);
+                        adapter.setState('waterMote_'+ id +'.cw_mom', {val: (buf.readInt16LE(0))/10, ack: true});
+                        adapter.log.info('cw counter: ' + (buf.readInt16LE(4))/10);
+                        adapter.setState('waterMote_'+ id +'.cw_cum', {val: (buf.readInt16LE(4))/10, ack: true});
+                        adapter.log.info('ww_mom:'+ (buf.readInt16LE(6))/10);
+                        adapter.setState('waterMote_'+ id +'.ww_mom', {val: (buf.readInt16LE(6))/10, ack: true});
+                        adapter.log.info('ww counter: ' + (buf.readInt16LE(8))/10);
+                        adapter.setState('waterMote_'+ id +'.ww_cum', {val: (buf.readInt16LE(8))/10, ack: true});
+                        adapter.log.info('Voltage: ' + (buf.readInt16LE(10))/10);
+                        adapter.setState('waterMote_'+ id +'.batt', {val: (buf.readInt16LE(10))/10, ack: true});
+                    }
+                    }
+                }
+            });
+        }
     });
 
     // in this template all states changes inside the adapters namespace are subscribed
@@ -109,18 +280,6 @@ function main() {
 
     // same thing, but the state is deleted after 30s (getState will return null afterwards)
     adapter.setState('testVariable', {val: true, ack: true, expire: 30});
-
-
-
-    // examples for the checkPassword/checkGroup functions
-    adapter.checkPassword('admin', 'iobroker', function (res) {
-        console.log('check user admin pw ioboker: ' + res);
-    });
-
-    adapter.checkGroup('admin', 'admin', function (res) {
-        console.log('check group user admin group admin: ' + res);
-    });
-
 
 
 }
