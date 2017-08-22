66e921caf8314e61b2fd9652e2ba995198fd1c5c
Andrey Kuznetsoff <dfrozen@ya.ru>
2017-08-22 14:20:16 +0300

    Update words.js

--- admin/words.js
+++ admin/words.js
@@ -1,10 +1,10 @@
 // Dictionary (systemDictionary is global variable from adapter-settings.js)
 systemDictionary = {
-	"template adapter settings": {"de": "Beispiel", "ru": "Пример"},
-	"test1":                    {"en": "Test 1", "de": "Test 1", "ru": "Тест 1"},
-	"test2":                    {"en": "Test 2", "de": "Test 2", "ru": "Тест 2"},
+	"template adapter settings": {"de": "Beispiel", "ru": "РџСЂРёРјРµСЂ"},
+	"test1":                    {"en": "Test 1", "de": "Test 1", "ru": "РўРµСЃС‚ 1"},
+	"test2":                    {"en": "Test 2", "de": "Test 2", "ru": "РўРµСЃС‚ 2"},
 	"on save adapter restarts with new config immediately": {
 		"de": "Beim Speichern von Einstellungen der Adapter wird sofort neu gestartet.",
-		"ru": "Сразу после сохранения настроек драйвер перезапуститься с новыми значениями"
+		"ru": "РџСЂРё СЃРѕС…СЂР°РЅРµРЅРёРё РЅР°СЃС‚СЂРѕРµРє Р°РґР°РїС‚РµСЂР° РѕРЅ СЃСЂР°Р·Сѓ Р¶Рµ РїРµСЂРµР·Р°РїСѓСЃРєР°РµС‚СЃСЏ"
 	}
 };
