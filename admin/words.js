af1fb83b28a632135dadb87fb8fda8908fbf609f
foxthefox <github@wysiwis.net>
2018-01-19 23:24:47 +0100

    adminv3

---
+++ admin/words.js
@@ -0,0 +1,69 @@
+<!-- you have to define 2 functions in the global scope: -->
+
+    // Dictionary (systemDictionary is global variable from adapter-settings.js)
+    systemDictionary = {
+        "jeelink adapter settings": {
+            "en": "jeelink adapter settings",       
+            "de": "jeelink Adapter Einstellung",   
+            "ru": "Настройки jeelink Драйвера"
+            },
+        "name":{
+            "en": "Name",                          
+            "de": "Name",                           
+            "ru": "Имя сенсора"
+            },
+        "sid":{
+            "en": "received Sensor ID",                     
+            "de": "empfangene Sensor ID",                      
+            "ru": "received ID сенсора"
+            },
+        "stype":{
+            "en": "Sensor type",                   
+            "de": "Sensortyp",                      
+            "ru": "Typ сенсора"
+            },
+        "usid":{
+            "en": "unique sensor id",
+            "de": "eineindeutige Sensor ID",
+            "ru": "unique ID сенсора"
+            },    
+        "jeelink USB adapter settings": {
+            "en": "jeelink USB adapter settings",       
+            "de": "jeelink USB Einstellung",   
+            "ru": "Настройки jeelink USB"
+            },            
+        "valid sensor types:": {
+            "en": "valid sensor types:",       
+            "de": "verfügbare Sensortypen:",   
+            "ru": "valid sensor types:"
+            },
+        "serialport":{
+            "en": "serial port",
+            "de": "USB port",                       
+            "ru": "USB port"
+            },
+        "baudrate":{
+            "en": "baudrate",                       
+            "de": "Baudrate",                       
+            "ru": "baudrate"
+            },
+        "Sensor configuration":     {
+            "en": "Sensor configuration",          
+            "de": "Sensorkonfiguration",            
+            "ru": "config сенсора"
+            },
+         "command for stick setup":     {
+            "en": "command for USB-stick setup",          
+            "de": "Kommando an USB-Stick zur Konfiguration",            
+            "ru": "command for USB-stick setup"
+            },
+          "enable command":     {
+            "en": "enable command",          
+            "de": "Kommando verwenden",            
+            "ru": "enable command"
+            },
+        "on save adapter restarts with new config immediately": {
+            "en": "on save adapter restarts with new config immediately",
+            "de": "Beim Speichern von Einstellungen der Adapter wird sofort neu gestartet.",
+            "ru": "Сразу после сохранения настроек драйвер перезапуститься с новыми значениями"
+        }
