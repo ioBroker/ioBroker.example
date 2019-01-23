0bb8300c52dc33ad1c2953b6b97c3f1934d4165b
Al Calzone <d.griesel@gmx.net>
2019-01-23 16:06:23 +0000

    Update templates to creator version v1.7.0

---
+++ JavaScript/admin/words.js
@@ -0,0 +1,43 @@
+/*global systemDictionary:true */
+'use strict';
+
+systemDictionary = {
+
+    'template adapter settings': {
+        'en': 'Adapter settings for template',
+        'de': 'Adaptereinstellungen für template',
+        'ru': 'Настройки адаптера для template',
+        'pt': 'Configurações do adaptador para template',
+        'nl': 'Adapterinstellingen voor template',
+        'fr': "Paramètres d'adaptateur pour template",
+        'it': "Impostazioni dell'adattatore per template",
+        'es': 'Ajustes del adaptador para template',
+        'pl': 'Ustawienia adaptera dla template',
+        'zh-cn': 'template的适配器设置'
+    },
+    'option 1 description': {
+        'en': 'Option 1 is cool',
+        'de': 'Option 1 ist cool',
+        'ru': 'Вариант 1 - это круто',
+        'pt': 'Opção 1 é legal',
+        'nl': 'Optie 1 is cool',
+        'fr': "L'option 1 est cool",
+        'it': "L'opzione 1 è cool",
+        'es': 'La opción 1 es genial',
+        'pl': 'Opcja 1 jest fajna',
+        'zh-cn': '选项1很酷'
+    },
+    'option 2 description': {
+        'en': 'Option 2 is not',
+        'de': 'Option 2 nicht',
+        'ru': 'Вариант 2 не',
+        'pt': 'A opção 2 não é',
+        'nl': 'Optie 2 is dat niet',
+        'fr': "L'option 2 n'est pas",
+        'it': "L'opzione 2 no",
+        'es': 'La opción 2 no es',
+        'pl': 'Opcja 2 nie jest',
+        'zh-cn': '选项2不是'
+    }
+
+};
